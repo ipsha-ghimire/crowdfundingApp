bc935a4831c570fda6bdb1b5968d1ad36ec943e8
ipsha ghimire <ipshagh2000@gmail.com>
2023-05-28 12:42:06 +0545

    variables created

--- web3/contracts/Crowdfunding.sol
+++ web3/contracts/Crowdfunding.sol
@@ -2,5 +2,23 @@
 pragma solidity ^0.8.9;
 
 contract Crowdfunding {
-    constructor() {}
+
+struct Request{
+    string description;
+    address payable recipient; //pay garna 
+    uint value;
+    bool completed;
+    uint noOfVoters;
+    mapping(address=>bool) voters; //voters pugyo ya pugena
+}
+
+mapping (address=>uint) public contributors;
+mapping(uint=>Request) public requests;
+uint public numRequests;
+address public manager;
+uint public minimunContributions; //min contribution amt
+uint public deadline;
+uint public target;
+uint public raisedAmount;
+uint public noOfContributors //kati jana 
 }
