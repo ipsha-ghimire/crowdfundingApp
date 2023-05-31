3bd6bd035381c17fc32f010d9ed0a1b967d943b1
ipsha ghimire <ipshagh2000@gmail.com>
2023-05-31 08:41:44 +0545

    components created as different pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import {ChainId,ThirdwebProvider } from '@thirdweb-dev/react';
 import App from './app';
+import  './index.css'
 
 const root= ReactDOM.createRoot(document.getElementById('root'));
 
