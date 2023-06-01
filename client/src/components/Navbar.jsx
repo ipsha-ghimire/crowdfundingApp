40e66c3fddb6e85b199440a7feec18b9ed98dbdf
Tulsi Gautam <tulsigautam05@gmail.com>
2023-06-01 10:18:52 +0545

    profile button added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,15 +9,15 @@ const Navbar = () => {
   const [isActive, setIsActive] = useState('dashboard');
   const [toggleDrawer, setToggleDrawer] = useState(false);
   const address = '0x3c..'
-  
+
 
   return (
     <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
       <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]">
         <input type="text" placeholder="Search for campaigns" className="flex w-full font-epilogue font-normal text-[14px] placeholder:text-[#4b5264] text-white bg-transparent outline-none" />
-        
+
         <div className="w-[72px] h-full rounded-[20px] bg-[#4acd8d] flex justify-center items-center cursor-pointer">
-          <img src={search} alt="search" className="w-[15px] h-[15px] object-contain"/>
+          <img src={search} alt="search" className="w-[15px] h-[15px] object-contain" />
         </div>
       </div>
 
@@ -32,8 +32,14 @@ const Navbar = () => {
           }}
         /> */}
 
-        
-        </div>
+        <Link to="/profile">
+          <div className="w-[52px] h-[52px] rounded-full bg-[#2c2f32] flex justify-center items-center cursor-pointer">
+            <img src={thirdweb} alt="user" className="w-[60%] h-[60%] object-contain" />
+          </div>
+        </Link>
+
+
+      </div>
     </div>
     // <div>navbar</div>
   )
