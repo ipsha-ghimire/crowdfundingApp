3f0105cb69b01c6aca584bf38da6050cc29344f6
ipsha ghimire <ipshagh2000@gmail.com>
2023-06-02 11:31:27 +0545

    css changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import { Sidebar, Navbar } from './components';
 const App = () => {
     return (
 
-        <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row ">
+        <div className="relative sm:-8 p-4 bg-[#FFFFFF] min-h-screen flex flex-row ">
 
             <div className="sm:flex hidden mr-10 relative ">
                 <Sidebar />
@@ -22,7 +22,7 @@ const App = () => {
                     <Route path="/campaign-details/:id" element={<CampaignDetails />} />
                 </Routes>
             </div>
-            <p className="font-bold">hellopp</p>
+         
         </div>
 
 
