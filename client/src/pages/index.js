b7c2bc1ead1d8c73b316549a7ce4f7b74d4026dd
Tulsi Gautam <tulsigautam05@gmail.com>
2023-07-08 13:14:23 +0545

    modified

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,6 +1,6 @@
 export { default as Home } from './Home';
 export { default as Profile } from './Profile';
 export { default as CreateCampaign } from './CreateCampaign';
-export {default as CampaignDetails } from './Campaigndetails';
+export {default as CampaignDetails } from './CampaignDetails';
 export{ default as Refund} from './refund';
 export{ default as RefundDetails} from './refunddetails';
